f465e8f3b7f8530a3c99b106fae702aed6759ac2
priyanshu <priyanshuqpwp@gmail.com>
2023-08-28 18:18:07 +0530

    made the login api

--- index.js
+++ index.js
@@ -1,26 +1,71 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('./public'))
+const User = require("./models/user.js");
 
-app.get('/', (req,res)=>{
-    res.json({message:"all right"})
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("./public"));
+
+// APIs-
+app.get("/health", (req, res) => {
+  res.json({ message: "all right" });
+});
+
+app.post("/api/signup", async (req, res) => {
+    try {
+      const { firstName, lastName, email, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      let user = await User.findOne({ email });
+      if (user) {
+        res.json({ message: "User already exists" });
+      } else {
+        res.json({ message: "User created successfully" });
+        const newUser = new User({
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword,
+        });
+        await newUser.save();
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "An error occurred", error });
+    }
+  });
 
 
+
+// Error Handler-
+app.use((req, res, next) => {
+  const err = new Error("Route not found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
 
 app.listen(process.env.PORT, () => {
-    mongoose.connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-       .then(()=> console.log(`running on port ${process.env.PORT}`))
-       .catch(error => console.error(error))
-    
-})
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log(`Server running on http://localhost:${process.env.PORT}`))
+    .catch((error) => console.error(error));
+});
---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: "string", required: true },
+  lastName: { type: "string", required: true },
+  email: { type: "string", required: true, unique: true },
+  password: { type: "string", required: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
+
+
